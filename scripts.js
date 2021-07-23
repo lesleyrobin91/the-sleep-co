4160c635dd5a2f11f5fe7d3e00a929b886146ec5
lesleyrobin91 <85639756+lesleyrobin91@users.noreply.github.com>
2021-07-23 18:13:05 +0200

    adding popup + button gallery

--- scripts.js
+++ scripts.js
@@ -12,3 +12,14 @@ $( document ).ready(function() {
         duration: 500 // values from 0 to 3000, with step 50ms
       });
 });
+
+// initializing the pop-up gallery
+lightGallery(document.getElementById('lightgallery'), {
+  download: false,
+  speed: 500
+});
+
+lightGallery(document.getElementById('lightgallery-2'), {
+  download: false,
+  speed: 500
+});
